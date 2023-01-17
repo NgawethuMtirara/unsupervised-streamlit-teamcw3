54a241eb18c315df154782ac1e3e957e10bd1fbf
ihaki <andrewpharisihaki@gmail.com>
2023-01-18 01:10:24 +0300

    I have added more pages and started development of the home page

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -32,6 +32,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#import image and animation libraries
+import requests
+import streamlit_lottie
+from streamlit_lottie import st_lottie
+from PIL import Image
+
 # Custom Libraries
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
@@ -40,17 +46,87 @@ from recommenders.content_based import content_model
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
+#load image resources 
+vision_img = Image.open(r'resources/imgs/vision.jpg')
+mission_img = Image.open(r'resources/imgs/mission.jpg')
+
+#create a lottie loading algorithm
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+#load  lottie urls
+home_lottie = load_lottieurl('https://assets9.lottiefiles.com/private_files/lf30_lnlbyoqx.json') 
+
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Home","Our Team", "Data Overview", "Recommender System","Solution Overview"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
+
+    if page_selection == 'Home':
+        st.title('Hi there :wave: we are CW-3')
+        st.write('---')
+        st.write('###')
+        st.write("""\n We are a team of data scientists at [EXPLORE Data science academy](https://www.explore.ai/)  
+        We deliver accurate solutions to real world problems using data """)
+        #st_lottie(home_lottie)
+        st.write('---')
+        
+        st.header('Mission')
+        st.write('---')
+
+        left_column, right_column = st.columns((3,2))
+        st.write('---')
+
+        with left_column:
+            st.write("""To deliver accurate and applicable machine learning algorithms that solve 
+            real world problems. """)
+            st.write("""We aim to combine machine intelligence to human intelligence to come up with 
+            cutting edge solutions to localised real-world problems  """)
+
+        with right_column:
+            st.image(mission_img, use_column_width=True)
+        st.header('vision')
+        st.write('---')
+        st.write('###')
+
+        left_column, right_column=st.columns((3,2))
+        
+        with left_column:
+
+            st.write("""To become industry leaders in providing applicable solutions to problems using
+            data """)
+        
+        with right_column:
+            st.image(vision_img)
+
+        st.header('Contact us')
+        st.write('---')
+        # generate a html script for a contact form
+        contact_form = """
+				<form action="https://formsubmit.co/andrewpharisihaki@gmail.com" method="POST">
+     <input type="text" name="message" placeholder = "enter a message" required>
+     <input type="email" name="email" placeholder = "enter your email" required>
+     <button type="submit">Send</button>
+     """
+        st.markdown(contact_form, unsafe_allow_html=True)
+
+        #styling the contract form using a css file
+        def locall_css(filename):
+            with open(filename) as f:
+                st.markdown(f"<style>{f.read()}</style", unsafe_allow_html=True)
+
+        locall_css(("style/style.css"))
+
     if page_selection == "Recommender System":
         # Header contents
         st.write('# Movie Recommender Engine')
